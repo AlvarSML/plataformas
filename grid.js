0490b99e0f2a824bc911c5bc3f5674ec35af15b0
alvarsnow <alvarsnow@gmail.com>
2017-09-14 18:02:37 +0200

    Inicio
    Todo añadido

---
+++ grid.js
@@ -0,0 +1,90 @@
+var canvas = document.getElementById("myCanvas");
+var ctx= canvas.getContext("2d");
+
+var m = [];
+for (var i = 0; i < 40; i++){
+	m[i] = [];
+}
+
+var grid = {
+	h : canvas.heigth,
+	w : canvas.width,
+	mesh : m,
+	update: function() {
+		for (var y = 0; y < 40; y ++){
+			for (var x = 0; x < 40; x ++){
+				grid.mesh[x][y].draw();
+			}
+		}		
+	},
+	generate: function() {
+		for (var y = 0; y < 40; y ++){
+			for (var x = 0; x < 40; x ++){
+				grid.mesh[x][y] = new point((x*10),(y*10));
+				grid.mesh[x][y].draw();		
+			}
+		}
+		ctx.stroke();
+		grid.sides();
+		console.log("Mapa generado");
+	},
+	soft: function() {
+		for (var x = 1; x < 39; x++) {
+			for (var y = 1; y < 39; y++) {
+				if (puntosLados(x,y,grid.mesh[x][y].fill) < 4) {
+					grid.mesh[x][y].fill = !grid.mesh[x][y].fill;
+				}
+			}
+		}
+		grid.update();
+	},
+	sides: function() {
+		for (var y = 0; y < 40; y ++){
+			for (var x = 0; x < 40; x ++){
+				if(grid.mesh[x][y].x == 0 || grid.mesh[x][y].y == 0 || grid.mesh[x][y].x == 390 || grid.mesh[x][y].y == 390){
+					grid.mesh[x][y].fill = true;
+					grid.mesh[x][y].draw();
+				}
+			}
+		}
+	},
+	closerTileX: function(x,y){
+		var px = x / 10;
+		var py = y / 10;
+	}
+};
+
+function point(px,py) {
+	this.x = px;
+	this.y = py;
+	this.fill = Boolean(Math.floor(Math.random() * 2));
+	this.color = "rgb(0,0,0)";
+
+	this.draw = function(){
+		if (this.fill){
+			ctx.fillRect(this.x,this.y,10,10);
+			ctx.fillStyle = this.color;
+			ctx.stroke();
+		} else {
+			ctx.clearRect(this.x,this.y,10,10);
+		}
+
+	}
+};
+
+
+// cuenta el numero de puntos por lado
+function puntosLados(px,py,tipo){
+	var pts = 0;
+
+	if (px > 0 && py > 0){
+		for (var x = px - 1; x <= px + 1; x ++){
+			for(var y = py -1 ; y <= py + 1; y ++ ){
+				if (grid.mesh[x][y].fill == tipo){
+					pts++;
+				}
+			}
+		}
+	}
+	return pts;
+}
---
+++ mainLoop.js
@@ -0,0 +1,35 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+var mainLoop = {
+	idExeution : null,
+	lastRecord : 0,
+	aps: 0,
+	fps: 0,
+	iteration: function(tmpRecord){
+		mainLoop.idExeution = window.requestAnimationFrame(mainLoop.iteration);
+		mainLoop.update(tmpRecord);
+		mainLoop.draw();
+
+		if (tmpRecord - mainLoop.lastRecord > 999 ){
+			mainLoop.lastRecord = tmpRecord;
+			console.log("APS: " + mainLoop.aps + " | FPS: " + mainLoop.fps);
+			mainLoop.aps = 0;
+			mainLoop.fps = 0;
+		}
+	}, 
+	stop: function(){
+		
+	},
+	update: function(){
+		mainLoop.aps++;
+		player.gravity();
+
+	},
+	draw: function(){
+		mainLoop.fps++;
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		grid.update();
+		player.draw();
+	}
+};
