08798fd411b9e3b96e9511e6164432e8ba7939fc
alvarsnow <alvarsnow@gmail.com>
2017-09-19 01:54:51 +0200

    controles añadidos

--- controls.js
+++ controls.js
@@ -1,16 +1,47 @@
-var keyboard = {
-  keys : Array(),
-  keyPress : function(key) {
-    keyboard.keys.push(key);
-  },
-  keyPresed : function(key){
-    return (keyboard.keys.indexOf(key) !== -1);
-  },
-  keyRelease : function() {
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
-  },
-  arrRestart : function() {
-    keyboard.keys = Array();
-  }
+var up = false,down = false,left = false,rigth = false;
 
-};
+function keyDownHandler(e) {
+   switch(e.keyCode) {
+      case 19,38,87:
+      console.log("Tecla up true");
+        up = true;
+        break;
+      case 37:
+      console.log("Tecla left true");
+        left = true;
+        break;
+      case 39:
+      console.log("Tecla rigth true");
+        rigth = true;
+        break;
+      case 40:
+      console.log("Tecla down true");
+        down = true;
+        break;
+    }
+  };
+
+
+function keyUpHandler(e) {
+    switch(e.keyCode) {
+      case 19,38,87:
+      console.log("Tecla up false");
+        up = false;
+        break;
+      case 37:
+      console.log("Tecla left false");
+        left = false;
+        break;
+      case 39:
+      console.log("Tecla rigth false");
+        rigth = false;
+        break;
+      case 40:
+      console.log("Tecla down false");
+        down = false;
+        break;
+    }
+  };
